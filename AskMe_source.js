b33768ac818d6c567485dcd64e4f91462b2663c2
SHIVAM TRIPATHI <shivamtripathi157@gmail.com>
2020-02-01 18:21:54 +0530

    Add files via upload

---
+++ AskMe_source.js
@@ -0,0 +1,78 @@
+var google = require("google");
+var axios = require("axios");
+const htmlToJson = require("html-to-json");
+var request = require('request');
+var cheerio = require('cheerio');
+
+google.resultsPerPage = 1;
+
+let self = this;
+
+const search = function (query, language) {
+  return new Promise((resolve, reject) => {
+    let searchString = `${query} in ${language} site:stackoverflow.com`;
+
+    google(searchString, (err, res) => {
+        if (err) {
+          reject({reason: 'A search error has occured :('})
+        }
+        else {
+          axios.get(res.url).then((response) => {
+              var promise = htmlToJson.parse(response.data,{
+                'link': function ($doc) {
+                    return $doc.find('div.kCrYT a').attr('href');
+                  }
+              }, function (err, result){
+                //ß  console.log(result);
+              });
+
+              promise.done(function(result){
+                var properResult = Object.assign(result);
+                resolve(properResult.link.replace('\/url?q=',''));
+              });
+          });
+        }
+    });
+  });
+}
+
+const scrape = function (html) {
+    $ = cheerio.load(html)
+    return $('div.accepted-answer pre code').text()
+}
+
+const download = function (url) {
+    return new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+          resolve(body)
+        } else {
+          reject({
+            reason: 'Unable to download page'
+          })
+        }
+      })
+    })
+  }
+
+const args = process.argv;
+console.log(args);
+
+let query = args[2].replace('\'','');
+let language = args[3].replace('\'','');
+
+search(query,language).then((url) => {
+  console.log(url);
+//  return self.download(url)
+   download(url).then((html) => {
+        let answer = scrape(html)
+        if (answer === '') {
+          console.log('No answer found :(');
+        } else {
+          console.log("\n" + "Code: " + "\n");
+          console.log(answer);
+        }
+      }).catch((error) => {
+          console.log(error);
+      })
+});
